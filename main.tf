0516ac7953c5c2935434e8bbddc9a150ff390f7f
Krishnasis Mandal <krishnasismandal97@gmail.com>
2024-09-04 18:34:30 +0200

    clean main

--- main.tf
+++ main.tf
@@ -5,16 +5,13 @@ terraform {
       version = "~> 5.0"
     }
   }
-}
-
-provider "aws" {
-  region = "eu-central-1"
-}
-
-terraform {
   backend "s3" {
     bucket = "terraformlocks"
     key    = "locks/bedrockconfigs"
     region = "eu-central-1"
   }
+}
+
+provider "aws" {
+  region = "eu-central-1"
 }
